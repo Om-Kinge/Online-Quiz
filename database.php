0bcd3ca762cdd969ef498797127bf67b9b631af6
Om Kinge <82507566+Om-Kinge@users.noreply.github.com>
2023-10-31 13:35:46 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
-$con= mysqli_connect('database-3.ckywwduexq4i.ap-south-1.rds.amazonaws.com','admin123','admin123','sample')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('database-3.ckywwduexq4i.ap-south-1.rds.amazonaws.com','admin123','admin123','sample')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
